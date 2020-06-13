001a0b4c30fc5391340072171a9381b762566272
sbharat99 <bsamineni@ufl.edu>
2020-06-12 20:31:11 -0400

    Modified main.py, player.py abnd table.py: New round intialization implemented with small and big blinds, Raise, Check and Fold also implemented

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from table import Table
 from deck import Deck
 from player import Player
 
+def print_balance(room1):
+    for player in room1.get_player_list():
+        print("Name: ", player.name, " ")
+        print("Hand: ", end="")
+        for h in player.hand:
+            print(h, end=" ")
+            print("Balance: ", player.balance)
+        
 
 def main():
     # room = Room()
@@ -24,25 +32,77 @@ def main():
 
     # print(test._deck.num_cards)
 
-    room = Room(123)
-    room.add_player(Player(12, "Adriel"))
-    room.add_player(Player(13, "Bharat"))
-    room.add_player(Player(14, "Yosh"))
-    room.add_player(Player(15, "Azhar"))
-    room._table.distribute_cards()
-    for player in room.get_player_list():
-            print("Name: ", player.name, " ")
-            print("Hand: ", end="")
-            for h in player.hand:
-                print(h, end=" ")
-                print("Balance: ", player.balance)
+    room1 = Room(123)
+    room1.add_player(Player(12, "Adriel"))
+    room1.add_player(Player(13, "Bharat"))
+    room1.add_player(Player(14, "Yosh"))
+    room1.add_player(Player(15, "Azhar"))
+    room1._table.distribute_cards()
 
-    print(room._table._deck.num_cards)
-    print(room._table._players)
-    room._table.new_round()
-    print(room._table.small_blind)
-    room._table.new_round()
-    print(room._table.small_blind)
+
+    print(room1._table._deck.num_cards)
+    print(room1._table._players)
+
+    # Round 1
+    room1._table.new_round()
+    print("Current Dealer: ", room1._table._dealer)
+    print("Current sb", room1._table._small_blind)
+    print("Current big Blind", room1._table._big_blind)
+
+    # room1._players[1].change_balance(-500)
+    # Round 2
+    # room1._table.new_round()
+    # print("Current Dealer: ", room1._table._dealer)
+    # print("Current sb", room1._table._small_blind)
+    # print("Current big Blind", room1._table._big_blind)
+
+    # Round 3
+    # room1._table.new_round()
+    # print("Current Dealer: ", room1._table._dealer)
+    # print("Current sb", room1._table._small_blind)
+    # print("Current big Blind", room1._table._big_blind)
+
+    while True:
+        player = room1._table.current_player
+        print("Current player is: ", player)
+        print(player, "Current Balance: ", player.balance, "\n")
+        if player.isFolded:
+            continue   
+        print(player.name)
+        print("Minimum Bet to Play:", room1._table.minimum_bet)
+        is_check = True if player.investment == room1._table.minimum_bet else False
+        checkOrCall = "Check" if is_check else "Call"
+        print("1.)", checkOrCall, "2.) Fold 3.) Raise")
+        option = int(input())
+        if(option == 1):
+            player.change_balance(-(room1._table.minimum_bet - player.investment))
+            room1._table.add_to_pot(room1._table.minimum_bet - player.investment)
+            player.add_investment(room1._table.minimum_bet - player.investment)
+            # print("Your Balance: ", player.balance)
+        if(option == 2):
+            # print("Your Balance: ", player.balance)
+            player.fold()
+        if(option == 3):
+            print("By how much?")
+            _raise = int(input())
+            room1._table.change_minimum_bet(_raise)
+            player.change_balance(-(room1._table.minimum_bet - player.investment))
+            room1._table.add_to_pot(room1._table.minimum_bet - player.investment)
+            player.add_investment(room1._table.minimum_bet - player.investment)           
+
+        print(player, " after Balance: ", player.balance, "\n")
+        room1._table.next_player() # ++player
+
+    
+    
+    room1._table._deck.pick_card() #the burn card
+    print(room1._table._deck.pick_card()) 
+    print(room1._table._deck.pick_card())  #The FLOP - three cards
+    print(room1._table._deck.pick_card())
+    
+
+
+
 
 
 # """            
