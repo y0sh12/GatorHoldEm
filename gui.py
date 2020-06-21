7cadac0d98289a3f0d8eebf56bdf957d7d2a90af
a.baig <a.baig@ufl.edu>
2020-06-20 20:51:33 -0400

    Added title to menu screens

--- gui.py
+++ gui.py
@@ -16,22 +16,28 @@ pygame.init()
 # Constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (0, 255, 0)
 
 # Variables
 clock = pygame.time.Clock()
 base_font = pygame.font.Font(None, 16)
-title = pygame.font.Font(None, 42)
+title_font = pygame.font.Font(None, 108)
 
 # Screen object creation
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('GatorHoldEm')
 
 
+# Function to create text objects
+
+
 # Loop for name prompt
 def intro_loop():
     # Variables
     user_text = ''
-    input_rect = pygame.Rect(300, 400, 100, 32)
+    input_rect = pygame.Rect(SCREEN_WIDTH * 3 / 8, SCREEN_HEIGHT * 2 / 3, 100, 32)
     color_active = pygame.Color('green')
     color_passive = pygame.Color('aquamarine3')
     color = color_passive
@@ -50,7 +56,7 @@ def intro_loop():
                         user_text = user_text[:-1]
 
                     elif event.key == K_RETURN:
-                        room_loop()
+                        room_name_loop()
 
                     else:
                         user_text += event.unicode
@@ -69,29 +75,34 @@ def intro_loop():
 
             # Check if window was closed
             elif event.type == QUIT:
-                print("Bye!!")
+                print("Game is being exited!!!")
                 intro = False
 
-        screen.fill((0, 0, 0))
+        screen.fill(black)
 
         if text_active:
             color = color_active
         else:
             color = color_passive
 
+        # Draw title on screen
+        title_surface = title_font.render("GatorHoldEm", True, white)
+        title_rect = title_surface.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4))
+        screen.blit(title_surface, title_rect)
+
+        # Draw input box on screen
         pygame.draw.rect(screen, color, input_rect, 1)
 
-        text_surface = base_font.render(user_text, True, (100, 255, 20))
-        screen.blit(text_surface, (input_rect.x + 10, input_rect.y + 10))
+        input_surface = base_font.render(user_text, True, (100, 255, 20))
+        screen.blit(input_surface, (input_rect.x + 10, input_rect.y + 10))
 
-        # input_rect.w = max(200, text_surface.get_width() + 10)
-        input_rect.w = 200
+        input_rect.w = max(200, input_surface.get_width() + 10)
 
         pygame.display.flip()
         clock.tick(60)
 
 
-def room_loop():
+def room_name_loop():
     # Variables
     user_text = "You're in a room!"
     input_rect = pygame.Rect(300, 400, 100, 32)
@@ -132,19 +143,25 @@ def room_loop():
                 print("Game is being exited!!!")
                 room = False
 
-        screen.fill((0, 0, 0))
+        screen.fill(black)
 
         if text_active:
             color = color_active
         else:
             color = color_passive
 
+        # Draw title on screen
+        title_surface = title_font.render("GatorHoldEm", True, white)
+        title_rect = title_surface.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4))
+        screen.blit(title_surface, title_rect)
+
+        # Draw input box on screen
         pygame.draw.rect(screen, color, input_rect, 1)
 
-        text_surface = base_font.render(user_text, True, (100, 255, 20))
-        screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+        input_surface = base_font.render(user_text, True, (100, 255, 20))
+        screen.blit(input_surface, (input_rect.x + 10, input_rect.y + 10))
 
-        input_rect.w = max(200, text_surface.get_width() + 10)
+        input_rect.w = max(200, input_surface.get_width() + 10)
 
         pygame.display.flip()
         clock.tick(60)
