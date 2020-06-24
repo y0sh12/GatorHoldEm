8a8b042398621d4aadae8ca2115aff6fe777aa4f
sbharat99 <bsamineni@ufl.edu>
2020-06-24 15:39:23 -0400

    Update SocketIOClient.py

--- SocketIOClient.py
+++ SocketIOClient.py
@@ -66,8 +66,9 @@ def on_event(ask):
 def main():
     global name
     name = input("What is your name?\n")
-    sio.connect('http://localhost:5000')
+    # sio.connect('http://localhost:5000')
     # sio.connect('http://172.105.150.126:5000')
+    sio.connect('http://45.33.96.41:5000')
     print('Your sid is', sio.sid)
     print("You are now in the lobby")
     room = input("What room would you like to join?\n")
